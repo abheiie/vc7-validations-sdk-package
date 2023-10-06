1c131fc549e08c59bf2e436a744f550ff8bd8c65
Abhishek Singh <abhisheksingh@ip-192-168-0-20.us-east-2.compute.internal>
2023-10-06 14:49:10 +0530

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='vc7-validations',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/ramsey009/vc7-validations-sdk-package.git',
+    author='Abhishek Singh',
+    author_email='colorles7@gmail.com',
+    license='unlicense',
+    packages=['vc7-validations'],
+    zip_safe=False
+)
+
